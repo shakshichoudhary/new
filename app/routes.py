4a5de45508106b6c194ba44d1cab5a27d4723945
unknown <choudharyshakshi198@gmail.com>
2019-06-27 10:22:55 +0530

    new project

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+    return '''
+    <html>
+    <head>
+     <title> HOME PAGE-Microblog</title>
+     <style>
+     h1{
+     color:blue
+     }
+     h2{
+     color:red
+     }
+     </style>
+     </head>
+     <body>
+     <h1 align=center>GOOD MORNING</h1>
+     <h2 align=center>we are doing practicals</h2>
+     <img:
+     </body>
+     </html>'''
+
+
+          
+
+    
+    
